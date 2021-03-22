8c2beac5199be2edc96c310807c63f22f703f276
AnWonYoung <ko1529@naver.com>
2021-03-22 16:00:13 +0900

    exam end

---
+++ TEST01/common.js
@@ -0,0 +1,34 @@
+// 첫 번째 문제 로그인
+function clklogin() {
+    var textId = document.getElementById('textId').value;
+    console.log('textId.value');
+    var textPw = document.getElementById('textPw').value;
+    console.log('textPw.value');
+
+    if(textId === '' || textPw === '') {
+        alert('로그인을 할 수 없습니다.')
+        return;
+    }
+    location.href = 'main.html'
+}  
+
+// 두 번째 문제 가입
+function memberjoin() {
+    var textId1 = document.getElementById('textId1').value;
+    console.log('textId1.value');
+    var textPw1 = document.getElementById('textPw1').value;
+    console.log('textPw1.value');
+    var textPwr = document.getElementById('textPwr').value;
+    console.log('textPwr.value');
+    if(textId1 === '' || textPw1 === '' || textPwr === '') {
+        alert("회원가입을 할 수 없습니다.");
+        return;
+    }
+    // location.href = ''
+
+    if(textPw1 !== textPwr) {
+        alert('비밀번호를 확인해주세요.');
+        return;
+    }
+    location.href = 'main.html'
+}
